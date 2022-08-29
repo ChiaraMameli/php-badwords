9f88a90819623c6a54d06f87ed623a1c53da3c45
ChiaraMameli <mameli.chiara@libero.it>
2022-08-29 12:58:46 +0200

    created string and added lenght

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php 
+/*
+Creiamo una variabile con un paragrafo di testo a scelta.
+Stampiamo a schermo il paragrafo e la sua lunghezza.
+Passiamo sulla barra del browser in query string una parola che vogliamo censurare
+Recuperiamo la parola da censurare via PHP e  sostituiamo tutte le sue occorrenze nel paragrafo con 3 asterischi.
+Stampiamo di nuovo il paragrafo e la sua nuova lunghezza.
+*/
+
+//Creiamo una variabile con un paragrafo di testo a scelta.
+$paragraph = 'Don Abbondio stava su una vecchia seggiola, ravvolto in una vecchia zimarra, con in capo una vecchia papalina. ';
+echo $paragraph;
+
+// Stampiamo a schermo il paragrafo e la sua lunghezza.
+echo strlen($paragraph);
+
+
 ?>
