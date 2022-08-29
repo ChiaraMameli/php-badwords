643c8090534a05337da0ea277eee66050f71eac7
ChiaraMameli <mameli.chiara@libero.it>
2022-08-29 13:03:27 +0200

    completed

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php 
-/*
-Creiamo una variabile con un paragrafo di testo a scelta.
-Stampiamo a schermo il paragrafo e la sua lunghezza.
-Passiamo sulla barra del browser in query string una parola che vogliamo censurare
-Recuperiamo la parola da censurare via PHP e  sostituiamo tutte le sue occorrenze nel paragrafo con 3 asterischi.
-Stampiamo di nuovo il paragrafo e la sua nuova lunghezza.
-*/
-
 // Creiamo una variabile con un paragrafo di testo a scelta.
 $paragraph = 'Don Abbondio stava su una vecchia seggiola, ravvolto in una vecchia zimarra, con in capo una vecchia papalina. ';
 echo $paragraph;
@@ -21,6 +13,3 @@ echo '<br></br>';
 $censored_word = $_GET['word'];
 echo $censored_paragraph = str_replace($censored_word, '*', $paragraph);
 echo strlen($censored_paragraph);
-
-
-?>
