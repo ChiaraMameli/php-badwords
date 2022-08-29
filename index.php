9571b4f05f1b4896f603daa61e7e48869a61a3ac
ChiaraMameli <mameli.chiara@libero.it>
2022-08-29 13:03:02 +0200

    completed

--- index.php
+++ index.php
@@ -7,12 +7,20 @@ Recuperiamo la parola da censurare via PHP e  sostituiamo tutte le sue occorrenz
 Stampiamo di nuovo il paragrafo e la sua nuova lunghezza.
 */
 
-//Creiamo una variabile con un paragrafo di testo a scelta.
+// Creiamo una variabile con un paragrafo di testo a scelta.
 $paragraph = 'Don Abbondio stava su una vecchia seggiola, ravvolto in una vecchia zimarra, con in capo una vecchia papalina. ';
 echo $paragraph;
 
 // Stampiamo a schermo il paragrafo e la sua lunghezza.
 echo strlen($paragraph);
+
+echo '<br></br>';
+
+// Recuperiamo la parola da censurare via PHP e sostituiamo tutte le sue occorrenze nel paragrafo con 3 asterischi.
+
+$censored_word = $_GET['word'];
+echo $censored_paragraph = str_replace($censored_word, '*', $paragraph);
+echo strlen($censored_paragraph);
 
 
 ?>
